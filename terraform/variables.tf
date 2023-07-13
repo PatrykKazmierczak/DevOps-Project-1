c3b54795f199eb63bf20bc27161cd4e257c41fe5
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-07-13 21:48:15 +0200

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,8 @@
 variable "region" {
     default = "eu-central-1"
 }
-variable "public_key" {
-  
-}
-variable "private_key" {
-  
-}
-variable "key_name" {
-  
-}
+variable "public_key" {}
+
+variable "private_key" {}
+
+variable "key_name" {}
