d9b4d1675ab69dc3fe1b28c1132fde8c804f77f0
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-07-13 21:08:21 +0200

    update

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app = express();
+const server = express();
 
 // Serve static files (HTML, CSS, JS)
-app.use(express.static('public'));
+server.use(express.static('public'));
 
 // Start the server
 const port = 8080;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
