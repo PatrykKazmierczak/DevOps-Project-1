8088635ec4cd366761a1fb1c2d93543837ebf663
Patryk Kaźmierczak <46955522+PatrykKazmierczak@users.noreply.github.com>
2023-08-01 16:50:12 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,6 +57,28 @@ resource "aws_security_group" "maingroup" {
       self             = false
       to_port          = 80
     }
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 9100
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 9100
+    }
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 9090
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 9090
+    }
   ]
 
 
