c0f8dd075e97f7c036fc9a9a484b5f4ff0883a80
Patryk Kaźmierczak <46955522+PatrykKazmierczak@users.noreply.github.com>
2023-08-01 16:41:50 +0200

    update

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,43 +1,5 @@
-/* const express = require('express');
-const server = express();
-
-// Serve static files (HTML, CSS, JS)
-server.use(express.static('public'));
-
-// Start the server
-const port = 8080;
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-}); */
-
-
 const express = require('express');
-const prometheus = require('prom-client');
-
 const server = express();
-
-// Create custom metrics
-const customMetric = new prometheus.Counter({
-  name: 'my_custom_metric',
-  help: 'A custom metric to monitor something',
-});
-
-// Example endpoint to increase the custom metric value
-server.get('/example', (req, res) => {
-  // Your application logic here
-  // ...
-
-  // Increase the custom metric value
-  customMetric.inc();
-
-  res.send('Example endpoint');
-});
-
-// Metrics endpoint to expose Prometheus metrics
-server.get('/metrics', (req, res) => {
-  res.set('Content-Type', prometheus.register.contentType);
-  res.end(prometheus.register.metrics());
-});
 
 // Serve static files (HTML, CSS, JS)
 server.use(express.static('public'));
@@ -47,6 +9,5 @@ const port = 8080;
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
 
 
